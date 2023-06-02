7a4751a7ffdf8dfb8ee5ceafd4e9bf8bf8baaf46
juanes <jamitocastillo@gmail.com>
2023-06-02 17:26:55 -0500

    Orientada a objetos

---
+++ actenclase/Objetos/doc.py
@@ -0,0 +1,27 @@
+class Persona:
+    def __init__(self,nombre,documento):
+        self.__nombre=nombre
+        self.__documento=documento
+        
+    def getNombre(self):
+        return self.__nombre
+    def setNombre(self,nombre):
+        self.__nombre=nombre
+    
+    def getDocumento(self):
+        return self.__documento
+    def setDocumento(self,documento):
+        self.__documento=documento        
+        
+    def mostrarDatos(self):
+        print('los datos de la persona son: ')
+        return self.__nombre, self.__documento    
+        
+p=Persona('Ana',123)
+#print(p.getNombre())
+q=Persona('Pedro',321)
+#print(q.getNombre())
+print(p.getDocumento())
+print(q.getDocumento())
+print(q.mostrarDatos())
+        
---
+++ actenclase/pila.py
@@ -0,0 +1,38 @@
+D1= []
+
+
+def push(val):
+    D1.append(val)
+
+
+def pop():
+    val = D1[-1]
+    del D1[-1]
+    return val
+
+
+push(1)
+push(2)
+push(3)
+push(4)
+
+print(pop())
+print(pop())
+print(pop())
+print(pop())
+
+class transporte:  # Definiendo la clase de la pila.
+    def __init__(self):  # Definiendo la función del constructor.
+        print("carro")
+objecto = transporte()  # Instanciando el objeto.
+
+class transporte:  # Definiendo la clase de la pila.
+    def __init__(self):  # Definiendo la función del constructor.
+        print("avion")
+objecto = transporte()  # Instanciando el objeto
+
+class transporte:  # Definiendo la clase de la pila.
+    def __init__(self):  # Definiendo la función del constructor.
+        print("tren")
+objecto = transporte()  # Instanciando el objeto
+
