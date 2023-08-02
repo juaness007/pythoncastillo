4b84d15d90ab0b0220e195a0f1663646fee1593b
juanes <jamitocastillo@gmail.com>
2023-08-02 17:28:43 -0500

    trabajando con archivos

---
+++ 3Trimestre/Samuel/ProcessArchivos/operaciones.py
@@ -0,0 +1,16 @@
+class operaciones:
+    def __init__(self,mayor,menor,promedio,media,mediana):
+        self.__mayor = mayor
+        self.__menor = menor
+        self.__promedio = promedio
+        self.__media=media
+        self.__mediana = mediana
+        
+    def mayor():
+        
+        
+    def menor():        
+        
+        
+        
+    
